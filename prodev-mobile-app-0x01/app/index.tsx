64e9f2423881a9c2040e92a1ac493148539db1ef
McDon419 <ebenezerdonkor890@gmail.com>
2025-10-01 16:14:17 +0000

    added extra text components with styles

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,17 +1,12 @@
 import { View, Text, StyleSheet } from "react-native";
 
-export default function Index() {
+export default function App() {
   return (
     <View style={styles.container}>
       <Text>Entry Screen - Awesome</Text>
-
       <View>
-        <Text style={styles.largeText}>
-          Typescript is great if you practice more
-        </Text>
-        <Text style={styles.mediumText}>
-          React Native provides you a single codebase for cross platforms
-        </Text>
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
         <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
     </View>
@@ -21,9 +16,6 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#90caf9",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
   largeText: {
     fontSize: 30,
